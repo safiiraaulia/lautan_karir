5b07b87c716963045f1b53168973cec8ab119e7d
Safira Aulia <112611988+safiiraaulia@users.noreply.github.com>
2025-12-15 11:59:04 +0700

    up

---
+++ app/Models/KepribadianDimensi.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KepribadianDimensi extends Model
+{
+    use HasFactory;
+}
